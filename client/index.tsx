198759970b6e1b65ee44e8c1d9f07b906b6bc310
PsychedelicMonkey <ryan.getz.96@gmail.com>
2022-08-09 12:36:09 -0600

    Redux

--- client/index.tsx
+++ client/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+
 import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,7 +11,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Button } from 'reactstrap';
+import { useAppSelector, useAppDispatch } from '../app/hooks';
+
+import { decrement, increment } from '../features/counter/counterSlice';
 
 const Home = () => {
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
   return (
     <div>
       <h1>Home Page</h1>
+
+      <Button onClick={() => dispatch(decrement())}>-</Button>
+      <h3>{count}</h3>
+      <Button onClick={() => dispatch(increment())}>+</Button>
     </div>
   );
 };
