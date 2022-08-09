6307a12e6999336bf3fe50ede25795eac01303a7
PsychedelicMonkey <ryan.getz.96@gmail.com>
2022-08-09 14:33:09 -0600

    Auth route validators

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response, Router } from 'express';
+import { check, validationResult } from 'express-validator';
 import { sign } from 'jsonwebtoken';
 
 import auth from '../middleware/auth';
@@ -9,8 +10,15 @@ const JWT_SECRET: string = process.env.JWT_SECRET || '';
 
 router.post(
   '/login',
+  check('email').isEmail().withMessage('please enter a valid email address'),
+  check('password').notEmpty().withMessage('please enter your password'),
   async (req: Request, res: Response, next: NextFunction) => {
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
       const { email, password } = req.body;
 
       const user = await User.findOne({ email }).select('+password').exec();
@@ -38,8 +46,23 @@ router.post(
 
 router.post(
   '/register',
+  check('name').notEmpty().withMessage('please enter your name'),
+  check('email').isEmail().withMessage('please enter a valid email address'),
+  check('password')
+    .isLength({ min: 5 })
+    .withMessage('password must be at least 5 characters long'),
+  check('email').custom((email) => {
+    return User.findOne({ email }).then(() => {
+      return Promise.reject('that email address is already registered');
+    });
+  }),
   async (req: Request, res: Response, next: NextFunction) => {
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
       const { name, email, password } = req.body;
 
       const user = await User.create({ name, email, password });
