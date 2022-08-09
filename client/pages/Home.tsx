6ca3057bc003bbb1c62470cf2c18f3bf2587e7cf
PsychedelicMonkey <ryan.getz.96@gmail.com>
2022-08-09 12:44:11 -0600

    Button flex

--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -2,19 +2,29 @@ import React from 'react';
 import { Button } from 'reactstrap';
 import { useAppSelector, useAppDispatch } from '../app/hooks';
 
-import { decrement, increment } from '../features/counter/counterSlice';
+import {
+  decrement,
+  increment,
+  selectCount,
+} from '../features/counter/counterSlice';
 
 const Home = () => {
-  const count = useAppSelector((state) => state.counter.value);
+  const count = useAppSelector(selectCount);
   const dispatch = useAppDispatch();
 
   return (
-    <div>
+    <div className="container">
       <h1>Home Page</h1>
 
-      <Button onClick={() => dispatch(decrement())}>-</Button>
-      <h3>{count}</h3>
-      <Button onClick={() => dispatch(increment())}>+</Button>
+      <div className="d-flex">
+        <Button className="mx-2" onClick={() => dispatch(decrement())}>
+          -
+        </Button>
+        <h3>{count}</h3>
+        <Button className="mx-2" onClick={() => dispatch(increment())}>
+          +
+        </Button>
+      </div>
     </div>
   );
 };
