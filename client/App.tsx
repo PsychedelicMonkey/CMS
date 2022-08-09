0cde1dfe406889e32b5d093d4c558f0e2a4d8839
PsychedelicMonkey <ryan.getz.96@gmail.com>
2022-08-09 10:56:56 -0600

    React user table

--- client/App.tsx
+++ client/App.tsx
@@ -1,18 +1,49 @@
 import React, { useEffect, useState } from 'react';
+import api from './lib/api';
 
 const App = () => {
-  const [msg, setMsg] = useState('');
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:5000/')
-      .then((res) => res.json())
-      .then((data) => setMsg(data.msg));
+    api.get('/users').then((res) => setUsers(res.data));
   }, []);
+
+  interface IUser {
+    _id: string;
+    name: string;
+    email: string;
+    createdAt: string;
+    updatedAt: string;
+  }
 
   return (
     <div>
-      <h1>React App</h1>
-      {msg ? <h3>{msg}</h3> : null}
+      <h1>Users</h1>
+
+      {users.length > 0 ? (
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Created At</th>
+              <th>Updated At</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user: IUser) => (
+              <tr key={user._id}>
+                <td>{user._id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.createdAt}</td>
+                <td>{user.updatedAt}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : null}
     </div>
   );
 };
